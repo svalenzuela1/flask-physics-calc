9669c5fc26a5cb3c129451e9b77c4734ce2842af
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-01-09 22:11:01 -0500

    html ready

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def fahrenheit_converter():
-    c_temp = ''
-    if request.method == 'POST' and 'c_temp' in request.form:
-        c_temp = (int(request.form.get('c_temp')) - 32) * 5/9
+    celsius = ''
+    if request.method == 'POST':
+        celsius = (int(request.form.get('celsius')) - 32) * 5/9
 
-    return render_template('index.html', c_temp=c_temp)
+    return render_template('index.html', celsius=celsius)
 
 
 if __name__ == '__main__':
