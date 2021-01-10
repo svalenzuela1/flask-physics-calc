a1dc92c7820200c39db4f95ee7fc3f5d5335c42f
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-01-09 21:57:59 -0500

    setting up fahrenheight_converter

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+
+def fahrenheit_converter():
+    c_temp = ''
+    if request.method == 'POST' and 'c_temp' in request.form:
+        c_temp = (int(request.form.get('c_temp')) - 32) * 5/9
+
+    return render_template('index.html', c_temp=c_temp)
 
 
 if __name__ == '__main__':
