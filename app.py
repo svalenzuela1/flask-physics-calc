df87097d238b123c17344515359ab59e6717aca1
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-01-21 02:50:37 -0500

    different html pages for different routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 
 app = Flask(__name__)
 
@@ -6,12 +6,28 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def fahrenheit_converter():
+    # resp = Flask.Response("Foo bar baz")
+    # resp.headers['Access-Control-Allow-Origin'] = '*'
+    c_temp = ''
     celsius = ''
     if request.method == 'POST':
-        celsius = (int(request.form.get('celsius')) - 32) * 5/9
+        c_temp = int(request.form.get('c_temp'))
+        celsius = round((c_temp - 32) * 5/9)
 
     return render_template('index.html', celsius=celsius)
 
+@app.route('/fahrenheit', methods=['GET', 'POST'])
 
-if __name__ == '__main__':
-    app.run()
+def celsius_converter():
+    f_temp = ''
+    fahrenheit = ''
+    if request.method == 'POST':
+        f_temp = int(request.form.get('f_temp'))
+        fahrenheit = round((f_temp - 32) * 5 / 9)
+
+    #return render_template('index.html', fahrenheit=fahrenheit)
+    return render_template('result.html', fahrenheit=fahrenheit)
+
+
+
+app.run()
