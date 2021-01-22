cf14d2e2c0b61e43101f91790d6eec4f4abbc6c3
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-01-21 21:46:25 -0500

    decided to use different routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -6,38 +6,33 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def fahrenheit_converter():
-    # resp = Flask.Response("Foo bar baz")
-    # resp.headers['Access-Control-Allow-Origin'] = '*'
-    #f_temp = ''
     c_temp = ''
-    celsius = ''
+    fahrenheit = ''
     if request.method == 'POST' and 'c_temp' in request.form:
         c_temp = int(request.form.get('c_temp'))
-        celsius = round((c_temp - 32) * 5/9)
+        fahrenheit = round((c_temp - 32) * 5/9)
         print('the prblem is python')
-    elif request.method == 'POST' and 'f_temp' in request.form:
-        celsius_converter()
-    # f_temp = ''
-    # fahrenheit = ''
-    # if request.method == 'POST':
-    #     f_temp = int(request.form.get('f_temp'))
-    #     fahrenheit = round((f_temp - 32) * 5 / 9)
 
+    # elif request.method == 'POST' and ValueError:
+    #     return '<h1>Please Enter Integer Value</h1>'
+    # elif request.method == 'POST' and 'f_temp' in request.form:
+    #     celsius_converter()
 
-    return render_template('index.html', celsius=celsius)
-
-# @app.route('/fahrenheit', methods=['GET', 'POST'])
-def celsius_converter():
-    f_temp = ''
-    fahrenheit = ''
-    if request.method == 'POST' and 'f_temp' in request.form:
-        f_temp = int(request.form.get('f_temp'))
-        fahrenheit = round((f_temp - 32) * 5 / 9)
-        print('the problem is html')
-
-    #return render_template('index.html', fahrenheit=fahrenheit)
     return render_template('index.html', fahrenheit=fahrenheit)
 
+@app.route('/fahrenheit', methods=['GET', 'POST'])
+
+def celsius_converter():
+    f_temp = ''
+    celsius = ''
+    if request.method == 'POST' and 'f_temp' in request.form:
+        f_temp = int(request.form.get('f_temp'))
+        celsius = round((f_temp - 32) * 5 / 9)
+    print('the problem is html')
+
+    return render_template('result.html', celsius=celsius)
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
+
