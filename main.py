37540d74b8803748044d2ac0ee7d08866639b10d
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-08 19:29:49 -0500

    adding home route

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+
+def home():
+    return render_template('home.html')
+
 
 @app.route('/celsius', methods=['GET', 'POST'])
 
